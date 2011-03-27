46987ecb605ca4c621b852b99157d44c9bafd331
Adam Sjøgren <asjo@koldfront.dk>
2011-03-27 16:22:00 +0200

    Use System.Time from old-time for a ~3x speed improvement.

--- contrib/timestamp.hs
+++ contrib/timestamp.hs
@@ -5,20 +5,19 @@
 -- Copyright (C) 2011, Adam Sjøgren <asjo@koldfront.dk>. Released under the GPLv3+.
 
 import System.IO
-import Data.Time
+import System.Time
 import System.Locale
 
 main :: IO ()
 main = do hSetBuffering stdin (LineBuffering)
-          t <- getCurrentTime
-          zone <- getTimeZone t
-          loop stdin zone
+          loop stdin
 
-loop :: Handle -> TimeZone -> IO ()
-loop h zone = do eof <- hIsEOF h
-                 if eof
-                   then return ()
-                   else do timestamp <- getCurrentTime
-                           string <- hGetLine h
-                           putStrLn $ formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S\t" (utcToZonedTime zone timestamp) ++ string
-                           loop h zone
+loop :: Handle -> IO ()
+loop h = do eof <- hIsEOF h
+            if eof
+              then return ()
+              else do clocktime <- getClockTime
+                      calendartime <- toCalendarTime clocktime
+                      string <- hGetLine h
+                      putStrLn $ formatCalendarTime defaultTimeLocale "%Y-%m-%d %H:%M:%S\t" calendartime ++ string
+                      loop h
