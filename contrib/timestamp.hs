241be4703084a4a524fb1cdb4d09baed7fad1063
Adam Sjøgren <asjo@koldfront.dk>
2011-06-06 21:25:08 +0200

    Use getContents and mapM_ instead of looping explicitly.

--- contrib/timestamp.hs
+++ contrib/timestamp.hs
@@ -1,24 +1,18 @@
 -- timestamp.hs
 --
---     $ ghc -Wall -O -dynamic -o timestamp timestamp.hs
+--     $ ghc -Wall -O -dynamic --make timestamp.hs
 --     $ strip timestamp
 --
 -- Copyright (C) 2011, Adam Sjøgren <asjo@koldfront.dk>. Released under the GPLv3+.
 
-import System.IO
 import System.Time
 import System.Locale
 
 main :: IO ()
-main = do hSetBuffering stdin LineBuffering
-          loop stdin
+main = do contents <- getContents
+          mapM_ timestampLn (lines contents)
 
-loop :: Handle -> IO ()
-loop h = do eof <- hIsEOF h
-            if eof
-              then return ()
-              else do clocktime <- getClockTime
-                      calendartime <- toCalendarTime clocktime
-                      string <- hGetLine h
-                      putStrLn $ formatCalendarTime defaultTimeLocale "%Y-%m-%d %H:%M:%S\t" calendartime ++ string
-                      loop h
+timestampLn :: String -> IO ()
+timestampLn l = do clocktime <- getClockTime
+                   calendartime <- toCalendarTime clocktime
+                   putStrLn $ formatCalendarTime defaultTimeLocale "%Y-%m-%d %H:%M:%S\t" calendartime ++ l
