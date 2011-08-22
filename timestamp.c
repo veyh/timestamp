7949471d387a4b5f0b8161ff44e165d07bf35a61
Adam Sjøgren <adsj@novozymes.com>
2011-08-22 13:24:49 +0200

    Add support for filename to append to; change FORMAT to an option (-f/--format).

--- timestamp.c
+++ timestamp.c
@@ -9,8 +9,12 @@
 #include <unistd.h>
 #include <string.h>
 #include <locale.h>
+#include <signal.h>
 
 #define BUFSIZE 128
+
+char *filename;
+FILE *out;
 
 void
 timestamp(char *fmt)
@@ -41,11 +45,35 @@ timestamp(char *fmt)
 	  _exit(1);
       }
 
-    if (fwrite(buf, 1, l, stdout) != l)
+    if (fwrite(buf, 1, l, out) != l)
 	_exit(1);
 
-    if (putchar('\t') == EOF)
+    if (fputc('\t', out) == EOF)
 	_exit(1);
+}
+
+void
+open_out(int signum)
+{
+    if (filename != NULL)
+      {
+	  if (out != NULL)
+	    {
+		fclose(out);
+	    }
+	  out = fopen(filename, "a");
+	  if (out == NULL)
+	    {
+		perror("timestamp");
+		_exit(10);
+	    }
+      }
+    else
+      {
+	  out = stdout;
+      }
+
+    setlinebuf(out);
 }
 
 int
@@ -53,21 +81,37 @@ main(int argc, char **argv)
 {
     int c, t;
     char *fmt = "%F %T";
+    int used = 0;
+    filename = NULL;
 
     (void) setlocale(LC_ALL, "");
 
     if (argc > 1)
       {
-	  if ((argc > 2) || (strcmp(argv[1], "-h") == 0)
+	  if ((strcmp(argv[1], "-h") == 0)
 	      || (strcmp(argv[1], "--help") == 0))
 	    {
-		(void) puts("Usage: timestamp [FORMAT]");
+		(void) puts("Usage: timestamp [-f FORMAT] [FILENAME]");
 		return 0;
 	    }
-	  fmt = argv[1];
+	  if ((strcmp(argv[1], "-f") == 0)
+	      || (strcmp(argv[1], "--format") == 0))
+	    {
+		fmt = argv[2];
+		used = 2;
+	    }
+	  if (argc > used)
+	    {
+		filename = argv[used + 1];
+	    }
       }
 
-    setlinebuf(stdout);
+    if (signal(SIGHUP, open_out) == SIG_IGN)
+      {
+	  signal(SIGHUP, SIG_IGN);
+      }
+
+    open_out(0);
 
     c = getchar();
 
@@ -90,7 +134,7 @@ main(int argc, char **argv)
 	    {
 		t = getchar();
 
-		if (putchar('\n') == EOF)
+		if (fputc('\n', out) == EOF)
 		    return 1;
 
 		if (t == EOF)
@@ -101,7 +145,7 @@ main(int argc, char **argv)
 
 		timestamp(fmt);
 	    }
-	  else if (putchar(c) == EOF)
+	  else if (fputc(c, out) == EOF)
 	      return 1;
       }
 }
