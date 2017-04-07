1a8dcc7b30b5eca5be533361bf08c90500556698
Klaus Alexander Seistrup <klaus@seistrup.dk>
2017-04-07 17:50:31 +0200

    Simplify things by using freopen()

--- timestamp.c
+++ timestamp.c
@@ -5,8 +5,9 @@
 */
 
 #include <stdio.h>
-#include <time.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
 #include <getopt.h>
 #include <locale.h>
@@ -36,64 +37,63 @@
 char *filename;
 FILE *out;
 
-void
+static void
+print(char *text, int end)
+{
+  if (fputs(text, stdout) == EOF)
+    exit(1);
+  if (putchar(end) == EOF)
+    exit(1);
+}
+
+static void
 timestamp(char *fmt)
 {
-  time_t t;
-  size_t l;
-  struct tm *tm;
+  time_t t = 0;
+  struct tm *tm = NULL;
   static char buf[BUFSIZE];
 
   t = time(NULL);
 
-  if (t == (time_t) - 1)
+  if (t == (time_t) -1)
     {
-      perror("Couldn't get current time");
-      _exit(1);
+      perror("Cannot get current time");
+      exit(1);
     }
 
   tm = localtime(&t);
 
   if (tm == (struct tm *) NULL)
-    _exit(-1);
+    exit(1);
 
-  if ((l = strftime(buf, BUFSIZE, fmt, tm)) == 0)
+  if (strftime(buf, BUFSIZE, fmt, tm) == 0)
     {
       (void) fprintf(stderr,
 		     "Resulting timestamp should be less than %d characters, please.\n",
 		     BUFSIZE);
-      _exit(1);
+      exit(1);
     }
 
-  if (fwrite(buf, 1, l, out) != l)
-    _exit(1);
-
-  if (fputc('\t', out) == EOF)
-    _exit(1);
+  print(buf, (int) '\t');
 }
 
-void
+static void
 open_out(int signum)
 {
   if (filename != NULL)
     {
-      if (out != NULL)
+      if ((out = freopen(filename, "a", stdout)) == (FILE *) NULL)
 	{
-	  fclose(out);
+	  perror(filename);
+	  exit(1);
 	}
-      out = fopen(filename, "a");
-      if (out == NULL)
-	{
-	  perror("timestamp");
-	  _exit(10);
-	}
-    }
-  else
-    {
-      out = stdout;
     }
 
-  setlinebuf(out);
+  if (setvbuf(stdout, NULL, (int) _IOLBF, 0) != 0)
+    {
+      perror("Cannot set line buffering");
+      exit(1);
+    }
 }
 
 int
@@ -169,9 +169,7 @@ main(int argc, char **argv)
     }
 
   if (signal(SIGHUP, open_out) == SIG_IGN)
-    {
-      signal(SIGHUP, SIG_IGN);
-    }
+    signal(SIGHUP, SIG_IGN);
 
   open_out(0);
 
@@ -192,9 +190,9 @@ main(int argc, char **argv)
       if (c == EOF)
 	return 0;
 
-      if (c == '\n')
+      if (c == (int) '\n')
 	{
-	  if (fputc('\n', out) == EOF)
+	  if (putchar((int) '\n') == EOF)
 	    return 1;
 
 	  t = getchar();
@@ -207,7 +205,7 @@ main(int argc, char **argv)
 
 	  timestamp(fmt);
 	}
-      else if (fputc(c, out) == EOF)
+      else if (putchar((int) c) == EOF)
 	return 1;
     }
 }
