6c00530425bd00a8536abad3aaba05862d11a508
Klaus Alexander Seistrup <klaus@seistrup.dk>
2017-04-10 10:08:52 +0200

    indent -gnu -npcs -i3

--- src/stampit.c
+++ src/stampit.c
@@ -73,210 +73,211 @@ int use_localtime = 0;
 static void
 timestamp(void)
 {
-  int res, usec;
-  int hh = 0, mm = 0, ss = 0;
-  time_t tnow = 0;
-  struct timespec ts = {
-    .tv_sec = 0,
-    .tv_nsec = 0
-  };
-  struct tm *tm = NULL;
+   int res, usec;
+   int hh = 0, mm = 0, ss = 0;
+   time_t tnow = 0;
+   struct timespec ts = {
+      .tv_sec  = 0,
+      .tv_nsec = 0
+   };
+   struct tm *tm = NULL;
 
-  res = clock_gettime(CLOCK_REALTIME, &ts);
+   res = clock_gettime(CLOCK_REALTIME, &ts);
 
-  if (res != 0)
-    {
-      perror("Cannot get current time");
-      exit(1);
-    }
+   if (res != 0)
+     {
+	perror("Cannot get current time");
+	exit(1);
+     }
 
-  tnow = ts.tv_sec;
-  usec = (int) ((ts.tv_nsec + 500) / 1000);
+   tnow = ts.tv_sec;
+   usec = (int) ((ts.tv_nsec + 500) / 1000);
 
-  while (usec > 1000000)
-    {
-      usec -= 1000000;
-      tnow += 1;
-    }
+   while (usec > 1000000)
+     {
+	usec -= 1000000;
+	tnow += 1;
+     }
 
 #ifdef __USE_MISC
-  if (use_localtime == 1)
-    {
-      tm = localtime(&tnow);
-      ss = tm->tm_gmtoff;
-      hh =  abs(ss) / 3600;
-      mm = (abs(ss) % 3600) / 60;
-    }
-  else
+   if (use_localtime == 1)
+     {
+	tm = localtime(&tnow);
+	ss = tm->tm_gmtoff;
+	hh =  abs(ss) / 3600;
+	mm = (abs(ss) % 3600) / 60;
+     }
+   else
 #endif
-    tm = gmtime(&tnow);
+      tm = gmtime(&tnow);
 
-  if (tm == (struct tm *) NULL)
-    exit(1);
+   if (tm == (struct tm *) NULL)
+      exit(1);
 
-  // YYYY-MM-DDThh:mm:ss.uuuuuu+00:00t
-  // ----+----|----+----|----+----|---
-  // = 32 chars for the stamp and 1 char for the TAB
-  res = printf("%04d-%02d-%02dT%02d:%02d:%02d.%06d%c%02d:%02d\t",
-	       tm->tm_year + 1900,
-	       tm->tm_mon + 1,
-	       tm->tm_mday,
-	       tm->tm_hour,
-	       tm->tm_min,
-	       tm->tm_sec,
-	       usec,
-	       ss < 0 ? '-' : '+', hh, mm);
+   // YYYY-MM-DDThh:mm:ss.uuuuuu+00:00t
+   // ----+----|----+----|----+----|---
+   // = 32 chars for the stamp and 1 char for the TAB
+   res = printf("%04d-%02d-%02dT%02d:%02d:%02d.%06d%c%02d:%02d\t",
+		tm->tm_year + 1900,
+		tm->tm_mon + 1,
+		tm->tm_mday,
+		tm->tm_hour,
+		tm->tm_min,
+		tm->tm_sec,
+		usec,
+		ss < 0 ? '-' : '+', hh, mm);
 
-  if (res != 33)
-    exit(1);
+   if (res != 33)
+      exit(1);
 }
 
 static void
 print(char *text, int end)
 {
-  if (fputs(text, stdout) == EOF)
-    exit(1);
-  if (putchar(end) == EOF)
-    exit(1);
+   if (fputs(text, stdout) == EOF)
+      exit(1);
+   if (putchar(end) == EOF)
+      exit(1);
 }
 
 static void
 open_out(int signum)
 {
-  if (filename != NULL)
-    {
-      if (freopen(filename, "a", stdout) == (FILE *) NULL)
-	{
-	  perror(filename);
-	  exit(1);
-	}
-    }
+   if (filename != NULL)
+     {
+	if (freopen(filename, "a", stdout) == (FILE *) NULL)
+	  {
+	     perror(filename);
+	     exit(1);
+	  }
+     }
 
-  if (setvbuf(stdout, NULL, (int) _IOLBF, 0) != 0)
-    {
-      perror("Cannot set line buffering");
-      exit(1);
-    }
+   if (setvbuf(stdout, NULL, (int) _IOLBF, 0) != 0)
+     {
+	perror("Cannot set line buffering");
+	exit(1);
+     }
 }
 
 int
 main(int argc, char **argv)
 {
-  int c, t;
-  int opt, idx = 0;
-  char *prog = basename(argv[0]);
+   int c, t;
+   int opt, idx = 0;
+   char *prog = basename(argv[0]);
 #ifdef __USE_MISC
-  char *short_opts = ":hvclo:";
+   char *short_opts = ":hvclo:";
 #else
-  char *short_opts = ":hvco:";
+   char *short_opts = ":hvco:";
 #endif
 
-  const struct option long_opts[] = {
-    {"help",      0, NULL, (int) 'h'},
-    {"version",   0, NULL, (int) 'v'},
-    {"copyright", 0, NULL, (int) 'c'},
-    {"output",    1, NULL, (int) 'o'},
-    { NULL,       0, NULL,        0 }
-  };
+   const struct option long_opts[] = {
+      {"help",      0, NULL, (int) 'h'},
+      {"version",   0, NULL, (int) 'v'},
+      {"copyright", 0, NULL, (int) 'c'},
+      {"output",    1, NULL, (int) 'o'},
+      { NULL,       0, NULL,        0 }
+   };
 
-  (void) setlocale(LC_ALL, "");
+   (void) setlocale(LC_ALL, "");
 
-  argv[0] = prog;
-  opterr = 0;
+   argv[0] = prog;
+   opterr = 0;
 
-  while ((opt = getopt_long(argc, argv, short_opts, long_opts, &idx)) != EOF)
-    {
-      switch (opt)
-	{
-	case 'h':
-	  (void) fprintf(stderr, USAGE, prog);
-	  return 0;
-	  /*@fallthrough@*/
-	case 'v':
-	  (void) fprintf(stderr, "%s/%s\n", PROGNAME, VERSION);
-	  return 0;
-	  /*@fallthrough@*/
-	case 'c':
-	  (void) fputs("GNU General Public License v3+\n", stderr);
-	  return 0;
-	  /*@fallthrough@*/
-	case 'o':
-	  filename = optarg;
-	  break;
+   while ((opt = getopt_long(argc, argv, short_opts, long_opts, &idx)) != EOF)
+     {
+	switch (opt)
+	  {
+	  case 'h':
+	     (void) fprintf(stderr, USAGE, prog);
+	     return 0;
+	     /*@fallthrough@ */
+	  case 'v':
+	     (void) fprintf(stderr, "%s/%s\n", PROGNAME, VERSION);
+	     return 0;
+	     /*@fallthrough@ */
+	  case 'c':
+	     (void) fputs("GNU General Public License v3+\n", stderr);
+	     return 0;
+	     /*@fallthrough@ */
+	  case 'o':
+	     filename = optarg;
+	     break;
 #ifdef __USE_MISC
-	case 'l':
-	  use_localtime = 1;
-	  break;
+	  case 'l':
+	     use_localtime = 1;
+	     break;
 #endif
-	default:
-	  if (optopt)
-	    {
-	      if (opt == (int) '?')
-		(void) fprintf(stderr, "%s: Invalid option ‘%c’.\n", prog,
-			       optopt);
-	      else if (opt == (int) ':')
-		(void) fprintf(stderr,
-			       "%s: Option ‘%c’ requires an argument.\n",
-			       prog, optopt);
-	    }
-	  else if (optind - 1 <= argc)
-	    (void) fprintf(stderr, "%s: Unrecognized option ‘%s’.\n",
-			   prog, argv[optind - 1]);
-	  (void) fprintf(stderr,
-			 "Try ‘%s --help’ for more information.\n", prog);
-	  return 1;
-	}
-    }
-  argv += optind;
-  argc -= optind;
+	  default:
+	     if (optopt)
+	       {
+		  if (opt == (int) '?')
+		     (void) fprintf(stderr, "%s: Invalid option ‘%c’.\n",
+				    prog, optopt);
+		  else if (opt == (int) ':')
+		     (void) fprintf(stderr,
+				    "%s: Option ‘%c’ requires an argument.\n",
+				    prog, optopt);
+	       }
+	     else if (optind - 1 <= argc)
+		(void) fprintf(stderr, "%s: Unrecognized option ‘%s’.\n",
+			       prog, argv[optind - 1]);
+	     (void) fprintf(stderr,
+			    "Try ‘%s --help’ for more information.\n",
+			    prog);
+	     return 1;
+	  }
+     }
+   argv += optind;
+   argc -= optind;
 
-  if (signal(SIGHUP, open_out) == SIG_IGN)
-    signal(SIGHUP, SIG_IGN);
+   if (signal(SIGHUP, open_out) == SIG_IGN)
+      signal(SIGHUP, SIG_IGN);
 
-  open_out(0);
+   open_out(0);
 
-  if (argc > 0)
-    {
-      timestamp();
+   if (argc > 0)
+     {
+	timestamp();
 
-      // Python: print(' '.join(argv))
-      for (idx = 0; idx < (argc - 1); ++idx)
-	print(argv[idx], (int) ' ');
+	// Python: print(' '.join(argv))
+	for (idx = 0; idx < (argc - 1); ++idx)
+	   print(argv[idx], (int) ' ');
 
-      print(argv[idx], (int) '\n');
+	print(argv[idx], (int) '\n');
 
-      return 0;
-    }
-
-  if ((c = getchar()) == EOF)
-    return 0;
-
-  if (ungetc(c, stdin) != c)
-    return 1;
-
-  timestamp();
-
-  for (;;)
-    {
-      if ((c = getchar()) == EOF)
 	return 0;
+     }
 
-      if (c == (int) '\n')
-	{
-	  if (putchar((int) '\n') == EOF)
-	    return 1;
+   if ((c = getchar()) == EOF)
+      return 0;
 
-	  if ((t = getchar()) == EOF)
-	    return 0;
+   if (ungetc(c, stdin) != c)
+      return 1;
 
-	  if (ungetc(t, stdin) != t)
-	    return 1;
+   timestamp();
 
-	  timestamp();
-	}
-      else if (putchar(c) == EOF)
-	return 1;
-    }
+   for (;;)
+     {
+	if ((c = getchar()) == EOF)
+	   return 0;
+
+	if (c == (int) '\n')
+	  {
+	     if (putchar((int) '\n') == EOF)
+		return 1;
+
+	     if ((t = getchar()) == EOF)
+		return 0;
+
+	     if (ungetc(t, stdin) != t)
+		return 1;
+
+	     timestamp();
+	  }
+	else if (putchar(c) == EOF)
+	   return 1;
+     }
 }
 
-// indent -gnu -npcs -i2
+// indent -gnu -npcs -i3
--- src/timestamp.c
+++ src/timestamp.c
@@ -82,268 +82,269 @@ int use_rfc3339 = 0;
 static void
 print(char *text, int end)
 {
-  if (fputs(text, stdout) == EOF)
-    exit(1);
-  if (putchar(end) == EOF)
-    exit(1);
+   if (fputs(text, stdout) == EOF)
+      exit(1);
+   if (putchar(end) == EOF)
+      exit(1);
 }
 
 static void
 timestamp(char *fmt)
 {
-  time_t tnow = 0;
-  struct timespec ts = {
-    .tv_sec = 0,
-    .tv_nsec = 0
-  };
-  struct tm *tm = NULL;
-  static char buf[BUFSIZE];
+   time_t tnow = 0;
+   struct timespec ts = {
+      .tv_sec  = 0,
+      .tv_nsec = 0
+   };
+   struct tm *tm = NULL;
+   static char buf[BUFSIZE];
 
-  if (clock_gettime(CLOCK_REALTIME, &ts) != 0)
-    {
-      perror("Cannot get current time");
-      exit(1);
-    }
+   if (clock_gettime(CLOCK_REALTIME, &ts) != 0)
+     {
+	perror("Cannot get current time");
+	exit(1);
+     }
 
-  tnow = ts.tv_sec;
+   tnow = ts.tv_sec;
 
 #ifdef __USE_MISC
-  if (use_rfc3339 == 1)
-    {
-      int hh, mm, ss, res;
-      int usec = (int) ((ts.tv_nsec + 500) / 1000);
+   if (use_rfc3339 == 1)
+     {
+	int hh, mm, ss, res;
+	int usec = (int) ((ts.tv_nsec + 500) / 1000);
 
-      while (usec > 1000000)
-	{
-	  usec -= 1000000;
-	  tnow += 1;
-	}
+	while (usec > 1000000)
+	  {
+	     usec -= 1000000;
+	     tnow += 1;
+	  }
 
-      if (use_utc == 1)
-	tm = gmtime(&tnow);
-      else
-	tm = localtime(&tnow);
+	if (use_utc == 1)
+	   tm = gmtime(&tnow);
+	else
+	   tm = localtime(&tnow);
 
-      if (tm == (struct tm *) NULL)
-	exit(1);
+	if (tm == (struct tm *) NULL)
+	   exit(1);
 
-      ss = tm->tm_gmtoff;
-      hh =  abs(ss) / 3600;
-      mm = (abs(ss) % 3600) / 60;
+	ss = tm->tm_gmtoff;
+	hh =  abs(ss) / 3600;
+	mm = (abs(ss) % 3600) / 60;
 
-      res = printf("%04d-%02d-%02dT%02d:%02d:%02d.%06d%c%02d:%02d\t",
-		   tm->tm_year + 1900,
-		   tm->tm_mon + 1,
-		   tm->tm_mday,
-		   tm->tm_hour,
-		   tm->tm_min,
-		   tm->tm_sec,
-		   usec,
-		   ss < 0 ? '-' : '+', hh, mm);
+	res = printf("%04d-%02d-%02dT%02d:%02d:%02d.%06d%c%02d:%02d\t",
+		     tm->tm_year + 1900,
+		     tm->tm_mon + 1,
+		     tm->tm_mday,
+		     tm->tm_hour,
+		     tm->tm_min,
+		     tm->tm_sec,
+		     sec,
+		     ss < 0 ? '-' : '+', hh, mm);
 
-      if (res != 33)  // 32 chars for the stamp + 1 TAB
-	exit(1);
-    }
-  else
-    {
+	if (res != 33)		// 32 chars for the stamp + 1 TAB
+	   exit(1);
+     }
+   else
+     {
 #endif
-      if (ts.tv_nsec >= 500000000)  // gmtime() and localtime()
-	tnow += 1;                  // don't round the seconds.
+	if (ts.tv_nsec >= 500000000)	// gmtime() and localtime()
+	   tnow += 1;		// don't round the seconds.
 
-      if (use_utc == 1)
-	tm = gmtime(&tnow);
-      else
-	tm = localtime(&tnow);
+	if (use_utc == 1)
+	   tm = gmtime(&tnow);
+	else
+	   tm = localtime(&tnow);
 
-      if (tm == (struct tm *) NULL)
-	exit(1);
+	if (tm == (struct tm *) NULL)
+	   exit(1);
 
-      if (strftime(buf, BUFSIZE, fmt, tm) == 0)
-	{
-	  (void) fprintf(stderr,
-			 "Resulting timestamp must have length 1‥%d.\n",
-			 BUFSIZE - 1);
-	  exit(1);
-	}
-      print(buf, (int) '\t');
+	if (strftime(buf, BUFSIZE, fmt, tm) == 0)
+	  {
+	     (void) fprintf(stderr,
+			    "Resulting timestamp must have length 1‥%d.\n",
+			    BUFSIZE - 1);
+	     exit(1);
+	  }
+	print(buf, (int) '\t');
 #ifdef __USE_MISC
-    }
+     }
 #endif
 }
 
 static void
 open_out(int signum)
 {
-  if (filename != NULL)
-    {
-      if (freopen(filename, "a", stdout) == (FILE *) NULL)
-	{
-	  perror(filename);
-	  exit(1);
-	}
-    }
+   if (filename != NULL)
+     {
+	if (freopen(filename, "a", stdout) == (FILE *) NULL)
+	  {
+	     perror(filename);
+	     exit(1);
+	  }
+     }
 
-  if (setvbuf(stdout, NULL, (int) _IOLBF, 0) != 0)
-    {
-      perror("Cannot set line buffering");
-      exit(1);
-    }
+   if (setvbuf(stdout, NULL, (int) _IOLBF, 0) != 0)
+     {
+	perror("Cannot set line buffering");
+	exit(1);
+     }
 }
 
 int
 main(int argc, char **argv)
 {
-  int c, t;
-  char *fmt = NULL;
-  int opt, idx = 0;
-  char *prog = basename(argv[0]);
+   int c, t;
+   char *fmt = NULL;
+   int opt, idx = 0;
+   char *prog = basename(argv[0]);
 #ifdef __USE_MISC
-  char *short_opts = ":hvcruf:";
+   char *short_opts = ":hvcruf:";
 #else
-  char *short_opts = ":hvcuf:";
+   char *short_opts = ":hvcuf:";
 #endif
 
-  const struct option long_opts[] = {
-    {"help",      0, NULL, (int) 'h'},
-    {"version",   0, NULL, (int) 'v'},
-    {"copyright", 0, NULL, (int) 'c'},
-    {"utc",       0, NULL, (int) 'u'},
+   const struct option long_opts[] = {
+      {"help",      0, NULL, (int) 'h'},
+      {"version",   0, NULL, (int) 'v'},
+      {"copyright", 0, NULL, (int) 'c'},
+      {"utc",       0, NULL, (int) 'u'},
 #ifdef __USE_MISC
-    {"rfc3339",   0, NULL, (int) 'r'},
+      {"rfc3339",   0, NULL, (int) 'r'},
 #endif
-    {"format",    1, NULL, (int) 'f'},
-    { NULL,       0, NULL,        0 }
-  };
+      {"format",    1, NULL, (int) 'f'},
+      { NULL,       0, NULL,        0 }
+   };
 
-  (void) setlocale(LC_ALL, "");
+   (void) setlocale(LC_ALL, "");
 
-  filename = NULL;
-  argv[0] = prog;
-  opterr = 0;
+   filename = NULL;
+   argv[0] = prog;
+   opterr = 0;
 
-  while ((opt = getopt_long(argc, argv, short_opts, long_opts, &idx)) != EOF)
-    {
-      switch (opt)
-	{
-	case 'h':
-	  (void) fprintf(stderr, USAGE, prog);
-	  return 0;
-	  /*@fallthrough@*/
-	case 'v':
-	  (void) fprintf(stderr, "%s/%s\n", PROGNAME, VERSION);
-	  return 0;
-	  /*@fallthrough@*/
-	case 'c':
-	  (void) fputs("GNU General Public License v3+\n", stderr);
-	  return 0;
-	  /*@fallthrough@*/
-	case 'f':
-	  fmt = optarg;
+   while ((opt = getopt_long(argc, argv, short_opts, long_opts, &idx)) != EOF)
+     {
+	switch (opt)
+	  {
+	  case 'h':
+	     (void) fprintf(stderr, USAGE, prog);
+	     return 0;
+	     /*@fallthrough@ */
+	  case 'v':
+	     (void) fprintf(stderr, "%s/%s\n", PROGNAME, VERSION);
+	     return 0;
+	     /*@fallthrough@ */
+	  case 'c':
+	     (void) fputs("GNU General Public License v3+\n", stderr);
+	     return 0;
+	     /*@fallthrough@ */
+	  case 'f':
+	     fmt = optarg;
 #ifdef __USE_MISC
-	  if (use_rfc3339 != 0)
-	    {
-	      (void) fprintf(stderr,
-			     "%s: Options ‘--format’ and ‘--rfc3339’ are mutually exclusive.\n",
-			     prog);
-	      return 1;
-	    }
-	  use_rfc3339 = 1;
-	  break;
-	case 'r':
-	  if (fmt != NULL)
-	    {
-	      (void) fprintf(stderr,
-			     "%s: Options ‘--rfc3339’ and ‘--format’ are mutually exclusive.\n",
-			     prog);
-	      return 1;
-	    }
-	  use_rfc3339 = 1;
+	     if (use_rfc3339 != 0)
+	       {
+		  (void) fprintf(stderr,
+				 "%s: Options ‘--format’ and ‘--rfc3339’ are mutually exclusive.\n",
+				 prog);
+		  return 1;
+	       }
+	     use_rfc3339 = 1;
+	     break;
+	  case 'r':
+	     if (fmt != NULL)
+	       {
+		  (void) fprintf(stderr,
+				 "%s: Options ‘--rfc3339’ and ‘--format’ are mutually exclusive.\n",
+				 prog);
+		  return 1;
+	       }
+	     use_rfc3339 = 1;
 #endif
-	  break;
-	case 'u':
-	  use_utc = 1;
-	  break;
-	default:
-	  if (optopt)
-	    {
-	      if (opt == (int) '?')
+	     break;
+	  case 'u':
+	     use_utc = 1;
+	     break;
+	  default:
+	     if (optopt)
+	       {
+		  if (opt == (int) '?')
+		     (void) fprintf(stderr,
+				    "%s: Invalid option ‘%c’.\n",
+				    prog, optopt);
+		  else if (opt == (int) ':')
+		     (void) fprintf(stderr,
+				    "%s: Option ‘%c’ requires an argument.\n",
+				    prog, optopt);
+	       }
+	     else if (optind - 1 <= argc)
 		(void) fprintf(stderr,
-			       "%s: Invalid option ‘%c’.\n",
-			       prog, optopt);
-	      else if (opt == (int) ':')
-		(void) fprintf(stderr,
-			       "%s: Option ‘%c’ requires an argument.\n",
-			       prog, optopt);
-	    }
-	  else if (optind - 1 <= argc)
-	    (void) fprintf(stderr,
-			   "%s: Unrecognized option ‘%s’.\n",
-			   prog, argv[optind - 1]);
-	  (void) fprintf(stderr,
-			 "Try ‘%s --help’ for more information.\n", prog);
-	  return 1;
-	}
-    }
-  argv += optind;
-  argc -= optind;
+			       "%s: Unrecognized option ‘%s’.\n",
+			       prog, argv[optind - 1]);
+	     (void) fprintf(stderr,
+			    "Try ‘%s --help’ for more information.\n",
+			    prog);
+	     return 1;
+	  }
+     }
+   argv += optind;
+   argc -= optind;
 
-  if (fmt == (char *) NULL)
-    fmt = "%F %T";
+   if (fmt == (char *) NULL)
+      fmt = "%F %T";
 
-  if (argc > 0)
-    {
-      if (strcmp(argv[0], "-") != 0)
-	filename = argv[0];
+   if (argc > 0)
+     {
+	if (strcmp(argv[0], "-") != 0)
+	   filename = argv[0];
 
-      --argc;
-      ++argv;
-    }
+	--argc;
+	++argv;
+     }
 
-  if (signal(SIGHUP, open_out) == SIG_IGN)
-    signal(SIGHUP, SIG_IGN);
+   if (signal(SIGHUP, open_out) == SIG_IGN)
+      signal(SIGHUP, SIG_IGN);
 
-  open_out(0);
+   open_out(0);
 
-  if (argc > 0)
-    {
-      timestamp(fmt);
+   if (argc > 0)
+     {
+	timestamp(fmt);
 
-      // Python: print(' '.join(argv), end=end)
-      for (idx = 0; idx < (argc - 1); ++idx)
-	print(argv[idx], (int) ' ');
+	// Python: print(' '.join(argv), end=end)
+	for (idx = 0; idx < (argc - 1); ++idx)
+	   print(argv[idx], (int) ' ');
 
-      print(argv[idx], (int) '\n');
-    }
+	print(argv[idx], (int) '\n');
+     }
 
-  if ((c = getchar()) == EOF)
-    return 0;
+   if ((c = getchar()) == EOF)
+      return 0;
 
-  if (ungetc(c, stdin) != c)
-    return 1;
+   if (ungetc(c, stdin) != c)
+      return 1;
 
-  timestamp(fmt);
+   timestamp(fmt);
 
-  for (;;)
-    {
-      if ((c = getchar()) == EOF)
-	return 0;
+   for (;;)
+     {
+	if ((c = getchar()) == EOF)
+	   return 0;
 
-      if (c == (int) '\n')
-	{
-	  if (putchar((int) '\n') == EOF)
-	    return 1;
+	if (c == (int) '\n')
+	  {
+	     if (putchar((int) '\n') == EOF)
+		return 1;
 
-	  if ((t = getchar()) == EOF)
-	    return 0;
+	     if ((t = getchar()) == EOF)
+		return 0;
 
-	  if (ungetc(t, stdin) != t)
-	    return 1;
+	     if (ungetc(t, stdin) != t)
+		return 1;
 
-	  timestamp(fmt);
-	}
-      else if (putchar((int) c) == EOF)
-	return 1;
-    }
+	     timestamp(fmt);
+	  }
+	else if (putchar((int) c) == EOF)
+	   return 1;
+     }
 }
 
-// indent -gnu -npcs -i2
+// indent -gnu -npcs -i3
